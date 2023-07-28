1c80fe82d18421695c1860a9d141d555b0207352
Maria Hola <maaria.hola@gmail.com>
2023-07-28 08:19:58 +0200

    second ex done

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ const array2 = [12, 30];
 
 const mergeArrays = (array1, array2) => [
 	...array1, 
-	...array2
+	...array2,
 ];
 
 console.log(mergeArrays);
 
+
+//ćwiczenie 2
+const elements = [2, "delfin", "akupresura", null];
+
+const exceptFirst = elements.slice(-3);
+
+console.log(exceptFirst);
 
